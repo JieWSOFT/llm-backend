16c94ef7b5800d765df49b74e2e0fd23ccb84a20
wj <murongyun8@gmail.com>
2024-12-18 14:56:53 +0800

    feat: 测试模板

--- backend/start.py
+++ backend/start.py
@@ -3,10 +3,11 @@ from core.config import settings
 if __name__ == "__main__":
     # 初始化模板
     import uvicorn
+
     uvicorn.run(
         app="main:app",
         host=settings.SEVER_HOST,
-        port=2132,
+        port=settings.SEVER_PORT,
         workers=4,
         reload=False if settings.ENVIRONMENT == "production" else True,
     )
