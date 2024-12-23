2b4d6c145efb7fafa5693149352f5067d007563d
wj <murongyun8@gmail.com>
2024-12-23 11:55:57 +0800

    feat: 允许跨域

--- backend/api/routes/bms/deps.py
+++ backend/api/routes/bms/deps.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, HTTPException, Request
+from loguru import logger
 
 
 ALLOWED_REFERER = "http://192.168.2.197:3333"
@@ -6,6 +7,7 @@ ALLOWED_REFERER = "http://192.168.2.197:3333"
 
 async def check_referer(request: Request):
     referer = request.headers.get("referer") or request.headers.get("origin")
+    logger.info(referer, ALLOWED_REFERER, referer != ALLOWED_REFERER)
     if referer != ALLOWED_REFERER:
         raise HTTPException(status_code=403, detail="Access forbidden from this source")
 
