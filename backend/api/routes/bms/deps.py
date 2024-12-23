7941cbe71eecc99a96978de767dab131030632fb
wj <murongyun8@gmail.com>
2024-12-23 11:59:20 +0800

    feat: 允许跨域

--- backend/api/routes/bms/deps.py
+++ backend/api/routes/bms/deps.py
@@ -7,7 +7,6 @@ ALLOWED_REFERER = "http://192.168.2.197:3333/"
 
 async def check_referer(request: Request):
     referer = request.headers.get("referer") or request.headers.get("origin")
-    logger.info(referer, ALLOWED_REFERER, referer != ALLOWED_REFERER)
     if referer != ALLOWED_REFERER:
         raise HTTPException(status_code=403, detail="Access forbidden from this source")
 
